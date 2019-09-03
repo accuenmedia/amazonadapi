9349b29b75154269d922226ded22d8886c734c21
jonjharrison <jj2harrison@gmail.com>
2019-09-03 15:42:43 -0700

    Separate set_region for connection

--- amazonadapi/connection.py
+++ amazonadapi/connection.py
@@ -2,17 +2,18 @@ import requests
 
 class Connection:
     def __init__(self, ad_client_id, ad_client_secret, profile_id, region, refresh_token):
-
         self.client_id = ad_client_id
         self.client_secret = ad_client_secret
         self.profile_id = profile_id
         self.refresh_token = refresh_token
 
+        self.set_region(region)
+
     def auto_refresh_token(self):
         i_sentinel = 1
         i_counter = 0
         while i_sentinel > 0:
-            get_token_url = "https://api.amazon.com/auth/o2/token"
+            get_token_url = self.host
             payload = {
                 "grant_type": "refresh_token",
                 "refresh_token": self.refresh_token,
@@ -31,3 +32,20 @@ class Connection:
             if i_counter >= 5:
                 i_sentinel = 0
         return results_json
+
+    def set_region(self, region='US'):
+        region_list = {
+            "UK": "https://api.amazon.co.uk/auth/o2/token",
+            "IN": "https://api.amazon.co.uk/auth/o2/token",
+            "US": "https://api.amazon.com/auth/o2/token",
+            "JP": "https://api.amazon.com.jp/auth/o2/token"
+        }
+        self.region = region
+        
+        try:
+            self.host = region_list[region]
+        except KeyError as e:
+            self.host = region_list["US"]
+            self.region = "US"
+
+        return self.host
