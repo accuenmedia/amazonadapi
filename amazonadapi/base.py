0a563a4e4d98eae27194250ac522b2c9bd848514
jonjharrison <jj2harrison@gmail.com>
2019-09-04 09:49:09 -0700

    Move order methods into order

--- amazonadapi/base.py
+++ amazonadapi/base.py
@@ -113,41 +113,6 @@ class Base:
         self.page_size = None
         return r
 
-    def get_orders(self, ad_id):
-        i_sentinel = 1
-
-        while i_sentinel == 1:
-            if self.page_token == None:
-                if self.page_size == None:
-                    url = "https://" + self.host + "/da/v1/advertisers/" + str(ad_id) + "/orders"
-                else:
-                    url = "https://" + self.host + "/da/v1/advertisers/" + str(ad_id) + "/orders?page_size=" + str(
-                        self.page_size)
-                    self.page_size = None
-            else:
-                url = "https://" + self.host + "/da/v1/advertisers/" + str(ad_id) + "/orders?page_token=" + self.page_token
-
-            r = self.make_request(url, self.object_headers, 'GET')
-            try:
-                self.next_page_url = r.headers['Link']
-            except:
-                i_sentinel = 0
-
-            if self.next_page_url != None:
-                p = re.compile('.*page_token=(.*)>')
-                matches = p.findall(self.next_page_url)
-                self.page_token = matches[0]
-
-        self.page_token = None
-        self.page_size = None
-        return r
-
-    def get_order(self, order_id):
-        url = "https://" + self.host + "/da/v1/orders/" + order_id
-        r = self.make_request(url, self.object_headers, 'GET')
-
-        return r
-
     def get_line_items(self, order_id):
         i_sentinel = 1
         while i_sentinel == 1:
@@ -181,20 +146,6 @@ class Base:
         url = "https://" + self.host + "/da/v1/line-items/" + line_item_id
         r = self.make_request(url, self.object_headers, 'GET')
 
-        return r
-
-    def create_order(self, order):
-        url = "https://" + self.host + "/da/v1/orders"
-        self.data = order
-
-        r = self.make_request(url, self.object_headers, 'POST', self.data)
-        return r
-
-    def update_order(self, order):
-        url = "https://" + self.host + "/da/v1/orders"
-        self.data = order
-
-        r = self.make_request(url, self.object_headers, 'PUT', self.data)
         return r
 
     def create_line_item(self, line_item):
